3fca083633048ee7a4880baeef6f4152a234b1ba
alicialf12 <alicia.asokawati@yahoo.com>
2024-06-04 07:47:32 +0800

    Membuat Halaman Sign Up

---
+++ lib/screens/signup_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_concept/screens/login_page.dart';
+import 'package:flutter_travel_concept/screens/main_screen.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(20.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.8,
+              ),
+              child: Card(
+                elevation: 5.0,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Register",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 20.0),
+                      TextButton(onPressed: (){
+                        Navigator.push(context, 
+                            MaterialPageRoute(builder: (context)=>LoginPage())
+                            );
+                      }, child: Text('Sudah Punya Akun? Login Disini')),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context, 
+                            MaterialPageRoute(builder: (context)=>MainScreen())
+                            );
+                        },
+                        child: Text('Daftar'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, 
+                          backgroundColor: Colors.green
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
