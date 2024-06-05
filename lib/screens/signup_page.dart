c7f555968dd32ffce97fa7caa92345d292182d1f
alicialf12 <alicia.asokawati@yahoo.com>
2024-06-06 01:27:03 +0800

    Update halaman register atau sign up

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_concept/screens/login_page.dart';
-import 'package:flutter_travel_concept/screens/main_screen.dart';
+import 'package:flutter_travel_concept/service/api_service.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -13,6 +13,31 @@ class _SignupPageState extends State<SignupPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController(); // Tambahkan controller untuk nama
+
+  Future<void> _register() async {
+    final String email = _emailController.text.trim();
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+    final String name = _nameController.text.trim(); // Ambil nilai nama dari input field
+
+    // Memanggil fungsi registerUser dari ApiService
+    bool success = await ApiService.registerUser(username, email, password, name, context);
+    if (success) {
+      // Registrasi berhasil
+      // Lakukan sesuatu jika diperlukan, misalnya tampilkan pesan sukses
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    } else {
+      // Registrasi gagal
+      // Tampilkan pesan kesalahan
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration failed')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,22 +93,30 @@ class _SignupPageState extends State<SignupPage> {
                         ),
                         obscureText: true,
                       ),
+                      SizedBox(height: 10.0), // Tambahkan jarak antara input field
+                      TextField( // Tambahkan input field untuk nama
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                       SizedBox(height: 20.0),
-                      TextButton(onPressed: (){
-                        Navigator.push(context, 
-                            MaterialPageRoute(builder: (context)=>LoginPage())
-                            );
-                      }, child: Text('Sudah Punya Akun? Login Disini')),
-                      ElevatedButton(
+                      TextButton(
                         onPressed: () {
-                          Navigator.push(context, 
-                            MaterialPageRoute(builder: (context)=>MainScreen())
-                            );
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => LoginPage()),
+                          );
                         },
+                        child: Text('Sudah Punya Akun? Login Disini'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _register,
                         child: Text('Daftar'),
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white, 
-                          backgroundColor: Colors.green
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.green,
                         ),
                       ),
                     ],
