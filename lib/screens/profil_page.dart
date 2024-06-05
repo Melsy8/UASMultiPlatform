767697509f00088b59dcf592f47d2aab6f8d6a9d
alicialf12 <alicia.asokawati@yahoo.com>
2024-06-06 01:33:12 +0800

    Update Halaman Profile

--- lib/screens/profil_page.dart
+++ lib/screens/profil_page.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  late String _username = 'Nama Pengguna';
+  late String _email = 'email@domain.com';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserInfo();
+  }
+
+  Future<void> _loadUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username') ?? 'Nama Pengguna';
+      _email = prefs.getString('email') ?? 'email@domain.com';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +36,11 @@ class ProfilePage extends StatelessWidget {
           children: <Widget>[
             CircleAvatar(
               radius: 50,
-              backgroundImage: AssetImage('assets/profile_picture.png'), // Path gambar profil
+              backgroundImage: AssetImage('assets/profile_picture.png'),
             ),
             SizedBox(height: 20),
             Text(
-              'Nama Pengguna',
+              _username,
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -25,36 +48,27 @@ class ProfilePage extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              'email@domain.com',
+              _email,
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.grey,
               ),
             ),
             SizedBox(height: 20),
-            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    print('Edit profile'); // Placeholder for actual action
+                    print('Edit profil'); // Aksi untuk mengedit profil
                   },
                   child: Text('Edit Profil'),
-                  style: ElevatedButton.styleFrom(
-                          foregroundColor: Color.fromARGB(255, 255, 255, 255), 
-                          backgroundColor: Colors.green
-                        ),
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    print('Change password'); // Placeholder for actual action
+                    print('Ubah kata sandi'); // Aksi untuk mengubah kata sandi
                   },
-                  child: Text('Ubah Password'),
-                  style: ElevatedButton.styleFrom(
-                          foregroundColor: Color.fromARGB(255, 255, 255, 255), 
-                          backgroundColor: Colors.green
-                        ),
+                  child: Text('Ubah Kata Sandi'),
                 ),
               ],
             ),
